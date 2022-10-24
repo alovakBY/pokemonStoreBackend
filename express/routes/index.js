397e12ee3c1630a77fc83ea405227625ac9b96a0
Artem_Loseu <alovakby@gmail.com>
2022-10-24 14:26:16 +0300

    fixed

--- express/routes/index.js
+++ express/routes/index.js
@@ -8,64 +8,64 @@ const cartRoutes = require("./cart.routes");
 
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 
-// router.use((req, res, next) => {
-//   try {
-//     if (
-//       req.url.startsWith("/auth") /* || req.url.startsWith("/set_pokemons") */
-//     ) {
-//       next();
-//     } else {
-//       const token = req.headers?.authorization?.split(" ")[1];
+router.use((req, res, next) => {
+  try {
+    if (
+      req.url.startsWith("/auth") /* || req.url.startsWith("/set_pokemons") */
+    ) {
+      next();
+    } else {
+      const token = req.headers?.authorization?.split(" ")[1];
 
-//       if (!token) {
-//         res.status(401).send({
-//           code: 401,
-//           message: "Not authorized",
-//         });
+      if (!token) {
+        res.status(401).send({
+          code: 401,
+          message: "Not authorized",
+        });
 
-//         res.end();
+        res.end();
 
-//         return;
-//       }
+        return;
+      }
 
-//       const jwtDecoded = jwt.decode(token);
+      const jwtDecoded = jwt.decode(token);
 
-//       if (jwtDecoded.exp * 1000 < new Date().getTime()) {
-//         res.status(401).send({
-//           code: 401,
-//           message: "Not authorized",
-//         });
+      if (jwtDecoded.exp * 1000 < new Date().getTime()) {
+        res.status(401).send({
+          code: 401,
+          message: "Not authorized",
+        });
 
-//         res.end();
+        res.end();
 
-//         return;
-//       }
+        return;
+      }
 
-//       next();
-//     }
-//   } catch (err) {
-//     res.status(401).send({
-//       code: 401,
-//       message: "Not authorized",
-//     });
-//   }
-// });
+      next();
+    }
+  } catch (err) {
+    res.status(401).send({
+      code: 401,
+      message: "Not authorized",
+    });
+  }
+});
 
-// router.use("/set_pokemons", async (req, res) => {
-//   await client.connect();
+router.use("/set_pokemons", async (req, res) => {
+  await client.connect();
 
-//   console.log(req.body);
+  console.log(req.body);
 
-//   const dbo = await client.db("pokemonStore");
+  const dbo = await client.db("pokemonStore");
 
-//   const collection = await dbo.collection("products");
+  const collection = await dbo.collection("products");
 
-//   const products = await collection.insertMany(req.body);
+  const products = await collection.insertMany(req.body);
 
-//   console.log(products);
+  console.log(products);
 
-//   res.end();
-// });
+  res.end();
+});
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello from Express.js!</h1>");
