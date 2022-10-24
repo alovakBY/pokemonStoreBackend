a2c5d4206a8d4393b5a7cb46c9824fc6d8119bcb
Artem_Loseu <alovakby@gmail.com>
2022-10-24 13:08:44 +0300

    fixed

--- express/server.js
+++ express/server.js
@@ -11,6 +11,14 @@ dotenv.config();
 
 const port = process.env.PORT || 8080;
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
