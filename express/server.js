370dc2b413b9b30768679420080bf7093fc7e95f
Artem_Loseu <alovakby@gmail.com>
2022-10-24 12:04:50 +0300

    fixed

--- express/server.js
+++ express/server.js
@@ -1,31 +1,22 @@
+"use strict";
 const express = require("express");
+const path = require("path");
 const serverless = require("serverless-http");
-
 const app = express();
+const bodyParser = require("body-parser");
+
 const router = express.Router();
-
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!",
-  });
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
 });
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
-router.get("/test", (req, res) => {
-  res.json({
-    hello: "test!",
-  });
-});
-
-router.post("/testpost", (req, res) => {
-  res.json({
-    hello: "hit the POST!",
-  });
-});
-
-app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../dist/index.html"))
-);
-app.use(`/.netlify/functions/api`, router);
+app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
