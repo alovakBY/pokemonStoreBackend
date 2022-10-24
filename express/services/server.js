08db5330855f9e498a9e0817d2b613311de2ad6b
Artem_Loseu <alovakby@gmail.com>
2022-10-24 12:20:10 +0300

    fixed

---
+++ express/services/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+const routes = require("./routes");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+// app.use("/", routes);
+app.use("/.netlify/functions/server", routes); // path must route to lambda
+// app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+// app.listen(port, () => {
+//   console.log(`server listening on port ${port}`);
+// });
+
+module.exports = app;
+module.exports.handler = serverless(app);
