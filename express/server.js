b5c1948c9bf18a8a453d47f8e0c52969683062fe
Artem_Loseu <alovakby@gmail.com>
2022-10-24 13:38:17 +0300

    fixed

--- express/server.js
+++ express/server.js
@@ -25,17 +25,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-// app.use(
-//   cors({
-//     origin: [
-//       "https://timely-mochi-9018a9.netlify.app/.netlify/functions/server/auth/signIn",
-//       "http://localhost:3002",
-//     ],
-//     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-//     credentials: true,
-//     origin: true,
-//   })
-// );
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
