56c94d0087f9fd4ce2d091e2b7d9a6f2e7933072
Artem_Loseu <alovakby@gmail.com>
2022-10-24 12:27:42 +0300

    fixed

--- express/routes/index.js
+++ express/routes/index.js
@@ -65,7 +65,7 @@ router.use((req, res, next) => {
 //   res.end();
 // });
 
-router.use("/", (req, res) => {
+router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello from Express.js!</h1>");
   res.end();
