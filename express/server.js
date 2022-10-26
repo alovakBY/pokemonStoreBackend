2d56181ec1edcc2ccf6652e050400ad5a7233604
Artem_Loseu <alovakby@gmail.com>
2022-10-26 11:53:09 +0300

    fixed

--- express/server.js
+++ express/server.js
@@ -26,10 +26,10 @@ app.use((req, res, next) => {
   );
   //    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-  //    res.setHeader(
-  //       "Access-Control-Allow-Headers",
-  //       "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Methods"
-  //    );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Methods"
+  );
   //    res.setHeader(
   //       "Access-Control-Allow-Methods",
   //       "POST, GET, PUT, OPTIONS, DELETE, HEAD"
